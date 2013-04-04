a4940f8df9410d479ace97ea2a3116afcf39ed55
fxmy <fxmywc@gmail.com>
2013-04-04 23:40:26 +0800

    exported loop/1,so that spawn_link/3 can be successed invoked;debug source

--- dmClientPreserver.erl
+++ dmClientPreserver.erl
@@ -1,10 +1,10 @@
 -module(dmClientPreserver).
--export([start/0,stop/0]).
+-export([start/0,stop/0,loop/1]).
 
 %% preservers a list of crashed dmServants' clients
 %% [sets] where set = tid()
 start() ->
-	PreserverPid = spawn_link(dmClientPreserver, loop, []),
+	PreserverPid = spawn_link(dmClientPreserver, loop, [dontCare]),
 	register(dmClientPreserver, PreserverPid),
 
 	PreserverPid ! init,
@@ -14,25 +14,25 @@ start() ->
 %% real work handled here.
 %% loop waits for a {'ETS-TRANSFER', tid(), FromPid, HeirData} to preserver a crashed dmServant
 %%                a {transOwner, Group, ServantPid} to return the SET
-loop( Preservered) ->
+loop( Preserved) ->
 	receive
 		init ->
 			%% Master send init signal, set up a empty SET of {channel, tid()}
 			PreservedTable = ets:new(preservedTable, [set]),
 			loop(PreservedTable);
 
-		{'ETS-TRANSFER', TableId, FromPid, HeirData} ->
+		{'ETS-TRANSFER', TableId, _FromPid, HeirData} ->
 			%% dmServat crashed
 			%% HeirData will be the group name
-			ets:insert(PreservedTable, {HeirData, TableId}),
-			loop(PreservedTable);
+			ets:insert(Preserved, {HeirData, TableId}),
+			loop(Preserved);
 
 		{transOwner, Group, ServantPid} ->
 			%% dmMaster sends signal to return the table back
-			[{_,TabID}] = ets:lookup(PreservedTable,Group),
+			[{_,TabID}] = ets:lookup(Preserved,Group),
 			ets:give_away(TabID, ServantPid, []),
-			ets:delete(PreservedTable, Group),
-			loop(PreservedTable)
+			ets:delete(Preserved, Group),
+			loop(Preserved)
 	end.
 
 stop() ->
